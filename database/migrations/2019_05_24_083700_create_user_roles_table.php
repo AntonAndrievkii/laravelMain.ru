ba6880a7b5f6f85fae98d5ddc2db073339692d9f
Admin <anton.rak.1996@mail.ru>
2020-01-27 01:41:51 +0300

    update database

---
+++ database/migrations/2019_05_24_083700_create_user_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('role_id')->unsigned()->default(2);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}
